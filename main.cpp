707b87c5e9fd8fb8b4d80575a648765f79f623c5
Lucas Owers <elpookyvg@gmail.com>
2024-03-13 15:23:05 +1030

    clear main

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Truckloads.h"
-#include <iostream>
-
-int main() {
-    Truckloads truckloads;
-    int trucksNeeded = truckloads.numTrucks(10, 2);
-    std::cout << "Number of trucks needed: " << trucksNeeded << std::endl;
-    return 0;
-}
