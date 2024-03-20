02cba45864001efe9738f163017afcc64ac9caef
Lucas Iain Owers <a1833619@embg13w048.ad.adelaide.edu.au>
2024-03-20 13:49:12 +1030

    man

--- r1.cpp
+++ r1.cpp
@@ -2,13 +2,21 @@
 #include<iostream>
 
 int sum_of_powers(int n){
+    int acc = 0;
     if (n==0){
-        return 0;
+        return acc;
     }
     else{
-    int acc = pow(n, n);
+    acc = acc+pow(n, n);
+    std::cout<<"Current acc: "<<acc<<std::endl;
     n = n-1;
-    return (sum_of_powers(n) + acc);
+    return (sum_of_powers(n)+acc);
     }
 }
+
+int main(){
+    sum_of_powers(4);
+    return 0;
+}
+
 
