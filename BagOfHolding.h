ce2b3df7fdc89bd3f6417736b409e518e36efe02
Lucas Iain Owers <a1833619@embg13w048.ad.adelaide.edu.au>
2024-03-20 14:44:29 +1030

    please '

---
+++ BagOfHolding.h
@@ -0,0 +1,15 @@
+#ifndef BAGGOFHOLDING_H
+#define BAGOFHOLDING_H
+
+#include<vector>
+
+class BagOfHolding{
+
+    private: std::vector<int> bag;
+    public:
+    void addItem();
+    void removeItem();
+    // something to do with templates.
+};
+
+#endif
