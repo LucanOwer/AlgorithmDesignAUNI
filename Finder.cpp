e6e4d4f26c79424717807078018f2995d3a8f70e
Lucas Iain Owers <a1833619@embg13w007.ad.adelaide.edu.au>
2024-03-27 14:33:47 +1030

    hello

---
+++ Finder.cpp
@@ -0,0 +1,24 @@
+#include "Finder.h"
+using namespace std;
+vector<int> Finder::findSubstrings(string s1, string s2)
+{
+    vector<int> result;
+    int index = 0;
+    for (size_t i = 1; i <= s2.size(); i++)
+    {
+        size_t found = s1.find(s2.substr(0, i), index);
+        if (found != string::npos)
+        {
+            result.push_back(found);
+            index = found;
+        }
+        else
+        {
+            for (size_t j=0; j<s2.size()-i+1; j++){
+            result.push_back(-1);
+            }
+        
+        }
+    }
+    return result;
+}
