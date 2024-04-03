06131cea3d51a23c1b75ce8f189d641709279b70
Lucas Owers <elpookyvg@gmail.com>
2024-04-03 13:52:09 +1030

    please

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
-#include "Truckloads.h"
 #include <iostream>
+#include "BubbleSort.cpp"
 
 int main() {
-    Truckloads truckloads;
-    int trucksNeeded = truckloads.numTrucks(10, 2);
-    std::cout << "Number of trucks needed: " << trucksNeeded << std::endl;
+    // Create an instance of BubbleSort
+    BubbleSort bubbleSort;
+
+    // Test data
+    std::vector<int> testData = {5, 2, 9, 1, 5, 6};
+
+    // Display original data
+    std::cout << "Original data: ";
+    for (int num : testData) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    // Sort the data using BubbleSort
+    std::vector<int> sortedData = bubbleSort.sort(testData);
+
+    // Display sorted data
+    std::cout << "Sorted data: ";
+    for (int num : sortedData) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
