ef03850a1e03b73e166929edee3f586893536887
Lucas Owers <elpookyvg@gmail.com>
2024-04-24 13:17:54 +0930

    daniels mother

---
+++ QuickSort.cpp
@@ -0,0 +1,9 @@
+#include "QuickSort.h"
+
+std::vector<int> QuickSort::quicksort(std::vector<int> &arr, int high, int low){
+    int pivot = 0;
+    if (high >= 3){
+        pivot = 3;
+
+
+}
---
+++ QuickSort.h
@@ -0,0 +1,11 @@
+#include "Sort.h"
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+class QuickSort : public Sort {
+public:
+    std::vector<int> sort(std::vector<int> list) override;
+    std::vector<int> quicksort(std::vector<int> &arr, int high,  int low);
+};
+
+#endif
