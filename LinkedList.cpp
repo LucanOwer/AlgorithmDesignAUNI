32815d3729dad3f4b7dfee76e13062618f1da198
Lucas Iain Owers <a1833619@embg13w048.ad.adelaide.edu.au>
2024-05-01 14:17:27 +0930

    yeahhhhh

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -35,7 +35,31 @@ void LinkedList::printList() {
 /////////// your code goes here... DO NOT change the function signatures ///////////
 
 bool LinkedList::swap(int pos1, int pos2) {
-	return true;
+	Node* currNode = head;
+	Node* pos1Node;
+	Node* pos2Node;
+	int data1;
+	int data2;
+	while (currNode != nullptr){
+		for (int i=0; i<pos1; i++){
+			currNode->link = pos1Node;
+		}
+		for (int j=0; j<pos2;j++){
+			currNode->link = pos2Node;
+		}
+		data1 = pos1Node->data;
+		data2 = pos2Node->data;
+
+		if (pos1Node->data != data2){
+			pos1Node->data = data2;
+		}
+
+		if (pos2Node->data!=data1){
+			pos2Node->data = data2;
+		}	
+		return true;
+	}
+	return false;
 }
 
 bool LinkedList::find_and_delete(int target) {
---
+++ main.cpp
@@ -0,0 +1,2 @@
+//dude like man like wow huh like cool -Lucas Owers
+// Sincererly, someone who doesn't know how to create a linked list in a main function.
